9bc7200ee14d0f9d77688088a7764572700e8eb6
Rohan <rohan.c.mathew@gmail.com>
2020-06-15 01:57:44 -0400

    buncha files

---
+++ static/js/main.js
@@ -0,0 +1,137 @@
+
+anime({
+
+	targets: 'canvas',
+	
+
+	translateX: [
+		{ value: 200, duration: 1000},
+		
+		
+	],
+
+
+
+
+
+});
+
+
+anime({
+
+	targets: document.getElementById('rcorners1'),
+	
+
+	translateX: [
+		{ value: 800, duration: 1000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+anime({
+
+	targets: document.getElementById('rcorners2'),
+	
+
+	translateX: [
+		{ value: 500, duration: 1000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+anime({
+
+	targets: document.getElementById('avatar'),
+	
+
+	translateX: [
+		{ value: -400, duration: 1000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+
+anime({
+
+	targets: document.getElementById('data'),
+	
+
+	translateX: [
+		{ value: 600, duration: 1000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+
+anime({
+
+	targets: document.getElementById('flag'),
+	
+
+	translateY: [
+		{ value: 1200, duration: 2000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+
+anime({
+
+	targets: 'p.rcorners2',
+	
+
+	translateX: [
+		{ value: 1000, duration: 1000},
+		
+		
+	],
+
+
+	
+  
+  easing: 'spring(1, 80, 10, 0)'
+
+
+});
+
+
